72405d970e5fec83b3656fb06c0b5dea11c285c3
fanqingyu <timo_quiet@163.com>
2020-11-09 16:03:37 +0800

    feat: 搜索组件和列表组件

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch'
+import FileList from './components/FileList'
 function App() {
+  const [ searchedFiles, setSearchedFiles ] = useState([])
+  const fileSearch = (keyword) => {
+    // filter out the new files based on the keyword
+    // const newFiles = filesArr.filter(file => file.title.includes(keyword))
+    // setSearchedFiles(newFiles)
+  }
   return (
     <div className="App container-fluid">
       <div className="row">
         <div className="col-3 bg-danger left-panel">
-          <h1>this is the left</h1>
+          <FileSearch
+            title='My Document'
+            onFileSearch={fileSearch}
+          />
+          <FileList
+            files={fileListArr}
+            onFileClick={fileClick}
+            onFileDelete={deleteFile}
+            onSaveEdit={updateFileName}
+          />
         </div>
         <div className="col-9 bg-info right-panel">
           <h1>this is the right</h1>
